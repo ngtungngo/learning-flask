9d0ec67b55ea64c23b981c935db227fd463687af
Tung Ngo <tung@micromata.de>
2022-01-31 20:18:25 +0100

    initial version

--- src/application.py
+++ src/application.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 
 @app.route("/")
---
+++ src/static/application.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $.ajaxSetup({
+        type: "GET",
+        data: {},
+        dataType: 'json',
+        xhrFields: {
+           withCredentials: true
+        },
+        crossDomain: true,
+        contentType: 'application/json; charset=utf-8'
+    });
+
+    $("#hello").click(function(event){
+        alert('Hallo')
+    });
+    
+});
+
+async function doSomeThing() {
+    console.log('some thing will be doing');
+    const res = await fetch('http://127.0.0.1:5000/api/user', {
+        mode: 'cors',
+        headers: {
+          'Access-Control-Allow-Origin':'*'
+        }
+      });
+    const json = await res.json();
+    console.log('user: ', json);
+}
