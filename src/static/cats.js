aa665b0b21876b16cda8c3dd9094d15953dd72db
Tung Ngo <tung@micromata.de>
2022-02-07 11:57:58 +0100

    use https://api.thecatapi.com/v1/images/search

--- src/static/cats.js
+++ src/static/cats.js
@@ -2,15 +2,8 @@
 $(document).ready(function() {
     const animals = document.getElementsByClassName('animal-img');
     for (let i = 0; i < animals.length; i++) {
-        fetch(`https://aws.random.cat/meow`)
-        .then((response) => data = response.json())
-        .then((data) => animals[i].src = data.file)
-        .catch((error) => console.log(error))
+        fetch('https://api.thecatapi.com/v1/images/search')
+        .then(response => response.json())
+        .then(data => animals[i].src = data[0].url);
     }
-    // document.getElementsByClassName('animal-img').forEach(img => {
-    //     fetch(`https://aws.random.cat/meow`)
-    //     .then((response) => img.src = response.json())
-    //     .then((data) => img.src = data.file)
-    //     .catch((error) => console.log(error))
-    // });
 });
